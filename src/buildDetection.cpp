5630042b56100136300780e010ba3f2b3ff54e4e
NataliaCarvalho03 <carvalho.natalia03@gmail.com>
2020-08-14 12:43:15 -0300

    codeAndData

---
+++ src/buildDetection.cpp
@@ -0,0 +1,69 @@
+#include <pcl/point_types.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/vfh.h>
+
+
+void estimate_normals()
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  ... read, pass in or create a point cloud ...
+
+  // Create the normal estimation class, and pass the input dataset to it
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+  ne.setInputCloud (cloud);
+
+  // Create an empty kdtree representation, and pass it to the normal estimation object.
+  // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+  ne.setSearchMethod (tree);
+
+  // Output datasets
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+
+  // Use all neighbors in a sphere of radius 3cm
+  ne.setRadiusSearch (0.03);
+
+  // Compute the features
+  ne.compute (*cloud_normals);
+
+  // cloud_normals->points.size () should have the same size as the input cloud->points.size ()*
+}
+
+
+void compute_vfh(){
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal> ());
+
+    ... read, pass in or create a point cloud with normals ...
+    ... (note: you can create a single PointCloud<PointNormal> if you want) ...
+
+    // Create the VFH estimation class, and pass the input dataset+normals to it
+    pcl::VFHEstimation<pcl::PointXYZ, pcl::Normal, pcl::VFHSignature308> vfh;
+    vfh.setInputCloud (cloud);
+    vfh.setInputNormals (normals);
+    // alternatively, if cloud is of type PointNormal, do vfh.setInputNormals (cloud);
+
+    // Create an empty kdtree representation, and pass it to the FPFH estimation object.
+    // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
+    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+    vfh.setSearchMethod (tree);
+
+    // Output datasets
+    pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs (new pcl::PointCloud<pcl::VFHSignature308> ());
+
+    // Compute the features
+    vfh.compute (*vfhs);
+
+    // vfhs->points.size () should be of size 1*
+}
+
+void read_cloud_from_txt(){
+
+
+}
+
+int main(){
+
+    return 0;
+}
